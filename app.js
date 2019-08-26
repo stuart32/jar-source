3ccebd2af347c047b7561d27262f1ddbb171f474
Stuart Fraser <stuartfraser@Stuarts-MacBook-Pro.local>
2019-08-26 13:16:38 +0100

    Added psid support

--- app.js
+++ app.js
@@ -35,15 +35,18 @@ app.post('/webhook', (req, res) => {
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-      // Get the webhook event. entry.messaging is an array, but 
-      // will only ever contain one event, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      // Gets the body of the webhook event
+  let webhook_event = entry.messaging[0];
+  console.log(webhook_event);
+
+  // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid);
       
     });
 
     // Return a '200 OK' response to all events
-    res.status(200).send('Hello Worlds');
+    res.status(200).send('Hello World!');
 
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
@@ -79,3 +82,18 @@ app.get('/webhook', (req, res) => {
     }
   }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
